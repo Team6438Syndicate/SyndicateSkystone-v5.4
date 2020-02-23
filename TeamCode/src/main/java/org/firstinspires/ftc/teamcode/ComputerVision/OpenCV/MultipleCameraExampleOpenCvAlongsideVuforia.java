338f18fd417ce936cac00d903f1848963c92e306
mattb <matthewbatkiewicz@gmail.com>
2020-02-23 18:45:29 -0500

    Flip webcam and increase RES

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ComputerVision/OpenCV/MultipleCameraExampleOpenCvAlongsideVuforia.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ComputerVision/OpenCV/MultipleCameraExampleOpenCvAlongsideVuforia.java
@@ -19,14 +19,13 @@
  * SOFTWARE.
  */
 
-package org.firstinspires.ftc.teamcode.OpenCV;
+package org.firstinspires.ftc.teamcode.ComputerVision.OpenCV;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
