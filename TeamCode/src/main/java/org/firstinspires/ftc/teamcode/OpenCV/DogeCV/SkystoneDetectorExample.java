00a23d746bef7d998e66524cb1b100d94ce6ac35
mattb <matthewbatkiewicz@gmail.com>
2020-02-25 13:59:25 -0500

    build.gradle updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV/DogeCV/SkystoneDetectorExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV/DogeCV/SkystoneDetectorExample.java
@@ -36,8 +36,6 @@ public class SkystoneDetectorExample extends LinearOpMode
          */
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
-
-
         // OR...  Do Not Activate the Camera Monitor View
         //webcam = new OpenCvInternalCamera(OpenCvInternalCamera.CameraDirection.BACK);
 
