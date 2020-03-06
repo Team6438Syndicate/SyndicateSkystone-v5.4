a64eadb1b48613aba40f916234c9a6cba1d93f74
mattb <matthewbatkiewicz@gmail.com>
2020-03-05 22:50:48 -0500

    Unmodified webcam class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/markmattcode/SimplifiedHardwareMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/markmattcode/SimplifiedHardwareMap.java
@@ -45,9 +45,7 @@ import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
 
 public class SimplifiedHardwareMap
 {
-
     //Vuforia Variables
-
     static final String VUFORIA_KEY = "ATEEWHn/////AAABmXzvuqxXZkYkr3AeTQT4Qg0P3tudpoBP/Rp2Xyw3zNlZYk+ZI5Jp/yo8TDf62o+UjdBvoe0LP5nNDqFESCtSImOG2WRuMkoESAyhSVzMU0hY53dWb4l0s7mCe+xqqT8i0r9pPdav7N7RiGHG7WYoIBXrQeyz+NEq8TLYTTCXmZMFgPeEU30Nb+t4JikoNMr0X0Ej6y1vG+7EX3O9KI8RXoPYbBmPzvX5uVvWBNg2J0g0SBiZUXa8pQOCxi0QyHyNUiwvV5WKnM2jncg+eI7im5s+k4yn6Xjaeecg6q9IT45YNvbhV4PM/LbwGQTKBf0AOCM/qL7tz7evypWw5uK15BayqAitBLy7Sr0SvIjYMjPg";
     static final double     COUNTS_PER_MOTOR_REV_DRIVE    = 383.6 ;    // eg: TETRIX Motor Encoder
     static final double     DRIVE_GEAR_REDUCTION    = 2.0 ;     // This is < 1.0 if geared UP
